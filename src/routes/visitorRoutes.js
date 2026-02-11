ec5b4ce3eb788c88b498f5620539d4433dd1b7cf
DeepanSenthilkumar <deepansenthilk2000@gmail.com>
2026-02-11 16:47:07 +0530

    getlist and update outtime API

--- src/routes/visitorRoutes.js
+++ src/routes/visitorRoutes.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { addVisitor } = require('../controllers/visitorController');
+const { addVisitor, updateOutTime, getVisitors } = require('../controllers/visitorController');
 
+// add a new data
 router.post('/add', addVisitor);
+
+// update out Time using Id
+router.put('/outTime/:visitorId', updateOutTime);
+
+router.post('/getVisitorList', getVisitors);
 
 module.exports = router;
